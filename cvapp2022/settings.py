1288a11198fcd8e7d4a49dffb0f019d0a9e7d7e6
jgardounis <gardounis-john@yahoo.com>
2022-12-16 15:25:46 +0200

    Update settings.py

--- cvapp2022/settings.py
+++ cvapp2022/settings.py
@@ -71,27 +71,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cvapp2022.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#    'default': {
-#        # 'ENGINE': 'django.db.backends.mysql',
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'cvappdb',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#        },
-# }
-DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 DATABASE_URL = os.environ['DATABASE_URL']
